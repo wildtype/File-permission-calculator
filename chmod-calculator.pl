08ec4c807cba448a96727fcc93cdc152585345ab
kriuk <kriuk>
2015-01-23 10:50:34 +0000

    ngetes event textbox auouoooo .

--- chmod-calculator.pl
+++ chmod-calculator.pl
@@ -7,11 +7,26 @@ use Gtk3 '-init';
 my $builder = Gtk3::Builder->new();
 $builder->add_from_file("interface.ui");
 $builder->connect_signals(undef);
+
 my $window = $builder->get_object("MainWindow");
 $window->show_all();
+
+my $cbUserRead = $builder->get_object("cbUserRead");
+my $tbOctal = $builder->get_object("tbOctal");
+
 Gtk3->main();
 
 sub on_MainWindow_delete_event
 {
     Gtk3::main_quit;
+}
+
+sub on_cbs_toggled
+{
+    my $isUserRead = $cbUserRead->get_active();
+    if($isUserRead) {
+        $tbOctal->set_text("User can read file");
+    } else {
+        $tbOctal->set_text("User can't read file");
+    }
 }
