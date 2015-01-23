efc9b569462f69d566af74a955133defc8b608a5
kriuk <kriuk>
2015-01-23 08:17:31 +0000

    Ganti ke Gtk3. Mix & match Gtk2::Builder plus tutorial gtk3 yang pake bhs C, bingung rek! Tapi udah bisa nampilin jendela.

--- chmod-calculator.pl
+++ chmod-calculator.pl
@@ -2,5 +2,13 @@
 
 use strict;
 use warnings;
-use Tk;
+use Gtk3 '-init';
 
+my $builder = Gtk3::Builder->new();
+$builder->add_from_file("interface-btn.glade");
+$builder->connect_signals(destroy=>sub{Gtk3::main_quit;});
+my $window = $builder->get_object("MainWindow");
+$window->show_all();
+Gtk3->main();
+
+exit;
